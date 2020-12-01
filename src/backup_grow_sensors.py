1bdd8360168af874f1c5fa882ad0af5bdd00829f
Michael Grant <mike.mgrant@gmail.com>
2020-11-30 19:39:58 -0800

    add backup code

---
+++ src/backup_grow_sensors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Sometimes the grower sensor csv file gets corrupted and reads 0 bytes. We want
+to backup this file but only if the size of the file is greater than
+0 bytes
+"""
+
+from datetime import datetime
+import logging
+import os
+import time
+
+
+SENSOR_PATH = "/home/pi/indoor_ag/data/sensors/saffron_grow_sensors.csv"
+SENSOR_PATH_BACKUP = "/home/pi/indoor_ag/data/sensors/saffron_grow_sensors_backup.csv"
+BACKUP_ERROR_LOG_PATH = "/home/pi/indoor_ag/logs/backup_file_error_log.txt"
+
+logging.basicConfig(
+    filename=BACKUP_ERROR_LOG_PATH,
+    filemode="w",
+    format="%(asctime)s - %(message)s",
+)
+
+current_file_size = os.stat(SENSOR_PATH).st_size
+
+while True:
+    now_dt = datetime.now()
+    # Make a backup twice an hour
+    if now_dt.minute in [00, 30]:
+        current_file_size = os.stat(SENSOR_PATH).st_size
+        if current_file_size < 1:
+            logging.error("Exception occurred", exc_info=True)
+        else:
+            os.system("cp {} {}".format(SENSOR_PATH, SENSOR_PATH_BACKUP))
+    time.sleep(20)
