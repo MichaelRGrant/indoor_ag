7e1b29cf0ed92d6608e903174829d6e67fc20586
Michael Grant <mike.mgrant@gmail.com>
2021-02-14 18:34:59 -0800

    remove images and push bash scripts

---
+++ bash_scripts/load_packages.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python3-dateutil python3-tables python3-sqlalchemy  -y
+
+sudo apt-get intall python3-numpy python3-scipy python3-sklearn python3-statsmodels python3-pandas -y
---
+++ bash_scripts/load_sensor_packages.sh
@@ -0,0 +1,16 @@
+# sudo apt-get install python-picamera python3-picamera -y
+
+
+pip3 install adafruit-circuitpython-ads1x15
+
+pip3 install adafruit-circuitpython-bme680
+
+pip3 install adafruit-circuitpython-ccs811
+
+pip3 install adafruit-circuitpython-tca9548a
+
+pip3 install adafruit-circuitpython-sht31d
+
+pip3 install adafruit-circuitpython-tsl2561
+
+
